08d70463ff38fd1c0d679df54fc0d07242a18bae
Rafael Rodrigues dos Santos <rafael11rodrigues@hotmail.com>
2018-06-03 23:02:28 -0300

    PAPI structure ready, no results yet

---
+++ volrend/instrumentado/srun.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+TIMEFORMAT=%R
+dir=$PWD
+EXECUTIONS=1
+files=("p_semaphore" "p_trans" "thread_mutex" "thread_spin" "thread_semaphore" 
+        "thread_trans" "tbb")
+
+mkdir output_papi
+
+for element in ${files[@]}; do
+	path="progs"/$element
+	cd $path
+	for t in 2 4 8 16; do
+		for ((i=1;i<=${EXECUTIONS};i++)); do
+			if [ "$element" = "tbb" ]
+        	then
+            	if [ "$t" -le "4" ]
+            	then
+                	echo -e "\n--> $element ($t flows) $i/${EXECUTIONS}\n"
+                	./build/app/main -i native -n $t >> ../../output_paralelo/tempo_${element}_${t}_threads.txt
+            	fi
+        	else
+            	echo -e "\n--> $element ($t flows) $i/${EXECUTIONS}\n"
+            	./build/app/main -i native -n $t >> ../../output_paralelo/tempo_${element}_${t}_threads.txt
+        	fi
+		done
+    done
+	cd -
+done
+
